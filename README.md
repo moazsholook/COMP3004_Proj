7040ef94051537df56d276e989572d17683c0d14
moazsholook <moazsholook@icloud.com>
2025-04-08 23:29:28 -0400

    readme changes

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 Insulin Pump Solution (Qt C++) 
 
 Members: 
-Moaz Sholook,
+Moaz Sholook 
 Mohammed Hassan,
 Fawwaz Ahmed,
 Rayan Ourdjini and
 Sam Marei
+
+Responsibilities:
+• Mohammed Hassan:  code implementation, UML Class Diagram, Testing
+• Fawwaz Ahmed: code Implementation, Textual Explanation, Traceability Matrix
+• Moaz Sholook: code Implementation, UML Sequence Diagram, Use Cases
+• Rayan Ordjini: code Implementation, UML Sequence Diagram, Use Cases
+• Sam Marei: code Implementation, State Machine Diagrams, Use Cases
+NEW
+
+
+Message Duddus, warrei, ehmetopaki, jini
+
 
 Youtube Video Link - https://youtu.be/IGqXgBZcv9Q?si=YkzzjVTT0C_cWBaa
--- build-Insulin_Pump-Desktop-Debug/moc_mainwindow.cpp
+++ build-Insulin_Pump-Desktop-Debug/moc_mainwindow.cpp
@@ -361,8 +361,8 @@ int ProfilesDialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     return _id;
 }
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata0[97];
+    QByteArrayData data[8];
+    char stringdata0[112];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -372,15 +372,16 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 16), // "onOptionsClicked"
-QT_MOC_LITERAL(2, 28, 0), // ""
-QT_MOC_LITERAL(3, 29, 14), // "onBolusClicked"
-QT_MOC_LITERAL(4, 44, 17), // "onRechargeClicked"
-QT_MOC_LITERAL(5, 62, 15), // "onRefillClicked"
-QT_MOC_LITERAL(6, 78, 18) // "updateBatteryLevel"
+QT_MOC_LITERAL(1, 11, 14), // "updateProfiles"
+QT_MOC_LITERAL(2, 26, 0), // ""
+QT_MOC_LITERAL(3, 27, 16), // "onOptionsClicked"
+QT_MOC_LITERAL(4, 44, 14), // "onBolusClicked"
+QT_MOC_LITERAL(5, 59, 17), // "onRechargeClicked"
+QT_MOC_LITERAL(6, 77, 15), // "onRefillClicked"
+QT_MOC_LITERAL(7, 93, 18) // "updateBatteryLevel"
 
     },
-    "MainWindow\0onOptionsClicked\0\0"
+    "MainWindow\0updateProfiles\0\0onOptionsClicked\0"
     "onBolusClicked\0onRechargeClicked\0"
     "onRefillClicked\0updateBatteryLevel"
 };
@@ -392,7 +393,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -400,13 +401,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       1,    0,   44,    2, 0x0a /* Public */,
+       3,    0,   45,    2, 0x08 /* Private */,
+       4,    0,   46,    2, 0x08 /* Private */,
+       5,    0,   47,    2, 0x08 /* Private */,
+       6,    0,   48,    2, 0x08 /* Private */,
+       7,    0,   49,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -422,11 +425,12 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWindow *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->onOptionsClicked(); break;
-        case 1: _t->onBolusClicked(); break;
-        case 2: _t->onRechargeClicked(); break;
-        case 3: _t->onRefillClicked(); break;
-        case 4: _t->updateBatteryLevel(); break;
+        case 0: _t->updateProfiles(); break;
+        case 1: _t->onOptionsClicked(); break;
+        case 2: _t->onBolusClicked(); break;
+        case 3: _t->onRechargeClicked(); break;
+        case 4: _t->onRefillClicked(); break;
+        case 5: _t->updateBatteryLevel(); break;
         default: ;
         }
     }
@@ -462,13 +466,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 6;
     }
     return _id;
 }
